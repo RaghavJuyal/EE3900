3ada4248629a46b8e71a9ab87a8c1c332ea8f773
RaghavJuyal <80700040+RaghavJuyal@users.noreply.github.com>
2022-11-09 10:04:53 +0530

    Add files via upload

---
+++ cktsig/run.sh
@@ -0,0 +1,33 @@
+echo Raghav - EP20BTECH11018
+echo BASH SCRIPT FOR CKTSIG ASSIGNMENT
+
+cd codes
+echo Running python codes in
+pwd
+
+python3 e2.6.py
+echo e2.6.py ran successfully
+sleep 10
+
+python3 e3.4.py
+echo e3.4.py ran successfully
+sleep 10
+
+python3 e4.3.py
+echo e4.3.py ran successfully
+sleep 10
+
+python3 e4.7.py
+echo e4.7.py ran successfully
+sleep 10
+
+echo All python codes ran successfully
+
+cd ..
+
+echo Compiling tex file in
+pwd
+
+pdflatex main.tex
+xdg-open main.pdf
+echo THANK YOU
